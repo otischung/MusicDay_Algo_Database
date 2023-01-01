9971ea3f88723723665634d4ce2d991cf504de67
otischung <my072814638@gmail.com>
2023-01-01 22:47:48 +0800

    ver.1 add comment

--- generate_label_csv.py
+++ generate_label_csv.py
@@ -15,7 +15,7 @@ for i in folder_list:
     music_list.append(tmp)
 
 with open(label_filename, "w+") as f:
-    f.write('Title,Artist,Album,File Path,Cover Path,Lowest Pitch,First Quartile Pitch,Medium Pitch,Third Quartile Pitch,Highest Pitch\n')
+    f.write('Title,Artist,Album,File_Path,Cover_Path,First_Comment,Lowest_Pitch,First_Quartile_Pitch,Medium_Pitch,Third_Quartile_Pitch,Highest_Pitch\n')
     for folder in tqdm(music_list):
         write_cover = True
         for music in folder:
@@ -26,12 +26,16 @@ with open(label_filename, "w+") as f:
             title = metadata.tag.title
             artist = metadata.tag.artist
             album = metadata.tag.album
+            comment = metadata.tag.comments
             if write_cover:
                 if metadata.tag.images:
                     cover_bin_data = metadata.tag.images[0].image_data
                     with open(cover_path, "wb") as fcover:
                         fcover.write(cover_bin_data)
                     write_cover = False
-            if title is None or artist is None or album is None:
+            if title is None or artist is None or album is None or comment is None:
                 print(f"\nError, the file {music} has no information.\n", file=sys.stderr)
-            f.write(f'"{title}","{artist}","{album}","{music}","{cover_path}"\n')
+                comment = None
+            else:
+                comment = metadata.tag.comments[0].text
+            f.write(f'"{title}","{artist}","{album}","{music}","{cover_path}","{comment}"\n')
