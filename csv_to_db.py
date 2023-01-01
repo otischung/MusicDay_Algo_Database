14584890471efeb2650a98625fb11451aa938151
hfkfo <kevinpan0930>
2023-01-01 23:17:48 +0800

    ver.1 add comment

--- csv_to_db.py
+++ csv_to_db.py
@@ -7,5 +7,5 @@ table = pd.read_csv(csv_file)
 conn = sqlite3.connect('MusicDay.db')  #建立資料庫
 cursor = conn.cursor()
 cursor.execute('CREATE TABLE music(title, Artist, Album, File_Path, Cover_Path, First_Comment, Lowest_Pitch, First_Quartile_Pitch, Medium_Pitch, Third_Quartile_Pitch, Highest_Pitch)')  #建立資料表
-conn.commit()
+conn.commit() 
 table.to_sql('music', conn, if_exists='append', index=False)  #將csv資料加入
