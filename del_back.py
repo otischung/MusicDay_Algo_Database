fd977d655657a928a4b3bcc8858c5a17c4e3b0c0
otischung <my072814638@gmail.com>
2023-01-02 16:22:52 +0800

    ver.2 delete music background modify abs path to pwd; add spleeter to requirements.txt

--- del_back.py
+++ del_back.py
@@ -1,17 +1,19 @@
 import os
+import sys
 
-path = 'Music'
+from tqdm import tqdm
+
+path = "./Music"
 allFileList = os.listdir(path)
-for file in allFileList:
-        album_path = os.path.join(path,file)
-        music_list = os.listdir(album_path)
-        for music in music_list:
-            if os.path.splitext(music)[1] == '.mp3':
-                music_path = os.path.join(album_path,music)
-                music_path = "\"" + music_path + "\""
-                output_path = 'D:\MusicDay_Algo_Database'
-                output_path = os.path.join(output_path,album_path)
-                output_path = "\"" + output_path + "\""
-                print(music_path)
-                os.system( f"python -m spleeter separate -p spleeter:2stems -o {output_path} {music_path}" )
-            
+for file in tqdm(allFileList):
+    album_path = os.path.join(path, file)
+    music_list = os.listdir(album_path)
+    for music in music_list:
+        if os.path.splitext(music)[1] == ".mp3":
+            music_path = os.path.join(album_path, music)
+            music_path = "\"" + music_path + "\""
+            output_path = os.getcwd()
+            output_path = os.path.join(output_path, album_path)
+            output_path = "\"" + output_path + "\""
+            print(music_path, file=sys.stderr)
+            os.system(f"python -m spleeter separate -p spleeter:2stems -o {output_path} {music_path}")
