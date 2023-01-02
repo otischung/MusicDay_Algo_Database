da2d002b86f09edb7876baf47d8b3133658fd9bf
hfkfo <kevinpan0930>
2023-01-02 14:28:00 +0800

    ver.1 add ID counter

--- csv_to_db.py
+++ csv_to_db.py
@@ -6,6 +6,6 @@ csv_file = "./label - 手改.csv"
 table = pd.read_csv(csv_file)
 conn = sqlite3.connect('MusicDay.db')  #建立資料庫
 cursor = conn.cursor()
-cursor.execute('CREATE TABLE music(title, Artist, Album, File_Path, Cover_Path, First_Comment, Lowest_Pitch, First_Quartile_Pitch, Medium_Pitch, Third_Quartile_Pitch, Highest_Pitch)')  #建立資料表
+cursor.execute('CREATE TABLE music(ID, title, Artist, Album, File_Path, Cover_Path, First_Comment, Lowest_Pitch, First_Quartile_Pitch, Medium_Pitch, Third_Quartile_Pitch, Highest_Pitch)')  #建立資料表
 conn.commit() 
 table.to_sql('music', conn, if_exists='append', index=False)  #將csv資料加入
